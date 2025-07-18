2c68e0294ace03ef78557e4b4a05cc6783178baa
German Bilalov <pinncho@gmail.com>
2025-07-18 22:49:32 +0200

    test(HttpError): use uppercase for constants in unit test

--- __tests__/utils/HttpError.test.tsx
+++ __tests__/utils/HttpError.test.tsx
@@ -3,14 +3,14 @@ import HttpError from '../../src/utils/HttpError';
 
 describe('HttpError', () => {
   it('creates an error with status and message', () => {
-    const NotFoundCode = 404;
-    const NotFoundText = 'NotFound';
-    const error = new HttpError(NotFoundCode, NotFoundText);
+    const NOT_FOUND = 404;
+    const NOT_FOUND_TEXT = 'NotFound';
+    const error = new HttpError(NOT_FOUND, NOT_FOUND_TEXT);
 
     expect(error).toBeInstanceOf(Error);
     expect(error).toBeInstanceOf(HttpError);
     expect(error.name).toBe('HttpError');
-    expect(error.status).toBe(NotFoundCode);
-    expect(error.message).toBe(NotFoundText);
+    expect(error.status).toBe(NOT_FOUND);
+    expect(error.message).toBe(NOT_FOUND_TEXT);
   });
 });
