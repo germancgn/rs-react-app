9df97ef2215fbd2b856e82e401513763fd42e562
German Bilalov <pinncho@gmail.com>
2025-07-18 23:08:49 +0200

    refactor: move mockMovies array to __mocks__/movies.ts

--- __tests__/components/MovieCard.test.tsx
+++ __tests__/components/MovieCard.test.tsx
@@ -1,36 +1,21 @@
 import { it, expect, describe, afterEach } from 'vitest';
 import MovieCard from '../../src/components/MovieCard';
 import { cleanup, render, screen } from '@testing-library/react';
-import type { Movie } from '../../src/types/movies/Movie';
 import '@testing-library/jest-dom/vitest';
-
-const movie: Movie = {
-  adult: false,
-  backdrop_path: '/path-to-backdrop.jpg',
-  id: 1,
-  title: 'Batman Begins',
-  original_title: 'Batman Begins',
-  overview:
-    'After training with his mentor, Batman begins his fight to free crime-ridden Gotham City.',
-  poster_path: '/path-to-poster.jpg',
-  media_type: 'movie',
-  original_language: 'en',
-  genre_ids: [28, 80],
-  popularity: 82.345,
-  release_date: '2005-06-15',
-  video: false,
-  vote_average: 8.2,
-  vote_count: 21000,
-};
+import { mockMovies } from '../../__mocks__/movies';
 
 afterEach(() => {
   cleanup();
 });
 
+const movie = mockMovies[0];
+
 describe('MovieCard', () => {
   it('renders the movie with the title as a heading', () => {
     render(<MovieCard movie={movie} />);
-    const heading = screen.getByRole('heading', { name: /Batman Begins/i });
+    const heading = screen.getByRole('heading', {
+      name: new RegExp(movie.title),
+    });
     expect(heading).toBeInTheDocument();
   });
 
