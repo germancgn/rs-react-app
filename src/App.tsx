9a1dc0ca6f5adc268c2b2f18e4e5940efe1a9a43
German Bilalov <pinncho@gmail.com>
2025-07-22 21:44:55 +0200

    refactor: extract spinner into separate component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ErrorBoundary from './components/ErrorBoundary';
 import { type Movie } from './types/movies/Movie';
 import { fetchPopularMovies, searchMovies } from './services/movie-service';
 import MoviesList from './components/MoviesList';
+import { Spinner } from './components/Spinner';
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
@@ -60,17 +61,7 @@ export default function App() {
             className="flex items-center justify-center h-screen"
             data-testid="loading-spinner"
           >
-            <span className="spin-animation">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="64"
-                height="64"
-                fill="#e94560"
-                viewBox="0 0 256 256"
-              >
-                <path d="M232,128a104,104,0,0,1-208,0c0-41,23.81-78.36,60.66-95.27a8,8,0,0,1,6.68,14.54C60.15,61.59,40,93.27,40,128a88,88,0,0,0,176,0c0-34.73-20.15-66.41-51.34-80.73a8,8,0,0,1,6.68-14.54C208.19,49.64,232,87,232,128Z"></path>
-              </svg>
-            </span>
+            <Spinner size={64} />
           </div>
         ) : (
           <ErrorBoundary>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { Spinner } from './Spinner';
+
 type HeaderProps = {
   searchTerm: string;
   onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -29,20 +31,7 @@ export default function Header({
               className="py-2 px-4 bg-[#e94560] text-white rounded-lg hover:bg-[#d13450] hover:cursor-pointer flex items-center gap-2"
               disabled={isLoading}
             >
-              Search{' '}
-              {isLoading && (
-                <span className="spin-animation" data-testid="spinner">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    viewBox="0 0 256 256"
-                  >
-                    <path d="M232,128a104,104,0,0,1-208,0c0-41,23.81-78.36,60.66-95.27a8,8,0,0,1,6.68,14.54C60.15,61.59,40,93.27,40,128a88,88,0,0,0,176,0c0-34.73-20.15-66.41-51.34-80.73a8,8,0,0,1,6.68-14.54C208.19,49.64,232,87,232,128Z"></path>
-                  </svg>
-                </span>
-              )}
+              Search {isLoading && <Spinner size={16} />}
             </button>
           </div>
         </div>
