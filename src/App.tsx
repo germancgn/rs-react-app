7d471aa2b4d2a8c9b0578c52df760a5f9881fba6
German Bilalov <pinncho@gmail.com>
2025-07-22 14:33:33 +0200

    refactor: convert class components to functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,86 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import ErrorBoundary from './components/ErrorBoundary';
 import { type Movie } from './types/movies/Movie';
 import { fetchPopularMovies, searchMovies } from './services/movie-service';
 import MoviesList from './components/MoviesList';
 
-type AppProps = Record<string, never>;
-type AppState = {
-  movies: Movie[];
-  isLoading: boolean;
-  searchTerm: string;
-};
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState(
+    localStorage.getItem('searchItem') ?? ''
+  );
 
-export default class App extends Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = {
-      movies: [],
-      isLoading: false,
-      searchTerm: localStorage.getItem('searchItem') ?? '',
-    };
-  }
-
-  componentDidMount(): void {
-    this.setState({ isLoading: true });
-    const searchTerm = this.state.searchTerm;
-    const fetchData = searchTerm
-      ? searchMovies(searchTerm)
-      : fetchPopularMovies();
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData =
+      (localStorage.getItem('searchItem') ?? '')
+        ? searchMovies(localStorage.getItem('searchItem') ?? '')
+        : fetchPopularMovies();
 
     fetchData
-      .then((data) => this.setState({ movies: data.results }))
-      .finally(() => this.setState({ isLoading: false }));
-  }
+      .then((data) => setMovies(data.results))
+      .finally(() => setIsLoading(false));
+  }, []);
 
-  handleSearch = () => {
-    const trimmedTerm = this.state.searchTerm.trim();
+  const handleSearch = () => {
+    const trimmedTerm = searchTerm.trim();
     if (!trimmedTerm) return;
 
     localStorage.setItem('searchItem', trimmedTerm);
 
-    this.setState({
-      isLoading: true,
-    });
+    setIsLoading(true);
 
     // setTimeout is used to simulate a slow connection
     setTimeout(() => {
       searchMovies(trimmedTerm)
         .then((data) => {
-          this.setState({ movies: data.results });
+          setMovies(data.results);
         })
         .finally(() => {
-          this.setState({ isLoading: false });
+          setIsLoading(false);
         });
     }, 1000);
   };
 
-  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     localStorage.setItem('searchItem', e.target.value);
-    this.setState({
-      searchTerm: e.target.value,
-    });
+    setSearchTerm(e.target.value);
   };
 
-  render() {
-    return (
-      <ErrorBoundary>
-        <div className="min-h-screen mx-auto bg-gradient-to-b from-[#1a1a2e] to-[#16213e] text-gray-300">
-          <Header
-            onSearch={this.handleSearch}
-            onInputChange={this.handleInputChange}
-            searchTerm={this.state.searchTerm}
-            isLoading={this.state.isLoading}
-          />
-          {this.state.isLoading ? (
-            <div
-              className="flex items-center justify-center h-screen"
-              data-testid="loading-spinner"
-            >
-              <span className="spin-animation">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="64"
-                  height="64"
-                  fill="#e94560"
-                  viewBox="0 0 256 256"
-                >
-                  <path d="M232,128a104,104,0,0,1-208,0c0-41,23.81-78.36,60.66-95.27a8,8,0,0,1,6.68,14.54C60.15,61.59,40,93.27,40,128a88,88,0,0,0,176,0c0-34.73-20.15-66.41-51.34-80.73a8,8,0,0,1,6.68-14.54C208.19,49.64,232,87,232,128Z"></path>
-                </svg>
-              </span>
-            </div>
-          ) : (
-            <ErrorBoundary>
-              <MoviesList movies={this.state.movies} />
-            </ErrorBoundary>
-          )}
-        </div>
-      </ErrorBoundary>
-    );
-  }
+  return (
+    <ErrorBoundary>
+      <div className="min-h-screen mx-auto bg-gradient-to-b from-[#1a1a2e] to-[#16213e] text-gray-300">
+        <Header
+          onSearch={handleSearch}
+          onInputChange={handleInputChange}
+          searchTerm={searchTerm}
+          isLoading={isLoading}
+        />
+        {isLoading ? (
+          <div
+            className="flex items-center justify-center h-screen"
+            data-testid="loading-spinner"
+          >
+            <span className="spin-animation">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="64"
+                height="64"
+                fill="#e94560"
+                viewBox="0 0 256 256"
+              >
+                <path d="M232,128a104,104,0,0,1-208,0c0-41,23.81-78.36,60.66-95.27a8,8,0,0,1,6.68,14.54C60.15,61.59,40,93.27,40,128a88,88,0,0,0,176,0c0-34.73-20.15-66.41-51.34-80.73a8,8,0,0,1,6.68-14.54C208.19,49.64,232,87,232,128Z"></path>
+              </svg>
+            </span>
+          </div>
+        ) : (
+          <ErrorBoundary>
+            <MoviesList movies={movies} />
+          </ErrorBoundary>
+        )}
+      </div>
+    </ErrorBoundary>
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,3 @@
-import { Component } from 'react';
-
 type HeaderProps = {
   searchTerm: string;
   onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -7,46 +5,47 @@ type HeaderProps = {
   isLoading: boolean;
 };
 
-export default class Header extends Component<HeaderProps> {
-  render() {
-    const { searchTerm, onInputChange, onSearch, isLoading } = this.props;
+export default function Header({
+  searchTerm,
+  onInputChange,
+  onSearch,
+  isLoading,
+}: HeaderProps) {
+  return (
+    <nav className="bg-white/10">
+      <div className="flex flex-col container m-auto max-w-6xl sm:flex-row  items-center justify-between gap-4 p-4 rounded-lg">
+        <h1 className="text-xl font-bold">Movie Search</h1>
+        <div className="flex max-sm:w-full sm:items-center gap-2">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={onInputChange}
+            placeholder="Search movies..."
+            className="w-full sm:w-64 p-2 bg-white/20 text-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#e94560]"
+          />
 
-    return (
-      <nav className="bg-white/10">
-        <div className="flex flex-col container m-auto max-w-6xl sm:flex-row  items-center justify-between gap-4 p-4 rounded-lg">
-          <h1 className="text-xl font-bold">Movie Search</h1>
-          <div className="flex max-sm:w-full sm:items-center gap-2">
-            <input
-              type="text"
-              value={searchTerm}
-              onChange={onInputChange}
-              placeholder="Search movies..."
-              className="w-full sm:w-64 p-2 bg-white/20 text-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#e94560]"
-            />
-
-            <button
-              onClick={onSearch}
-              className="py-2 px-4 bg-[#e94560] text-white rounded-lg hover:bg-[#d13450] hover:cursor-pointer flex items-center gap-2"
-              disabled={isLoading}
-            >
-              Search{' '}
-              {isLoading && (
-                <span className="spin-animation" data-testid="spinner">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    viewBox="0 0 256 256"
-                  >
-                    <path d="M232,128a104,104,0,0,1-208,0c0-41,23.81-78.36,60.66-95.27a8,8,0,0,1,6.68,14.54C60.15,61.59,40,93.27,40,128a88,88,0,0,0,176,0c0-34.73-20.15-66.41-51.34-80.73a8,8,0,0,1,6.68-14.54C208.19,49.64,232,87,232,128Z"></path>
-                  </svg>
-                </span>
-              )}
-            </button>
-          </div>
+          <button
+            onClick={onSearch}
+            className="py-2 px-4 bg-[#e94560] text-white rounded-lg hover:bg-[#d13450] hover:cursor-pointer flex items-center gap-2"
+            disabled={isLoading}
+          >
+            Search{' '}
+            {isLoading && (
+              <span className="spin-animation" data-testid="spinner">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  viewBox="0 0 256 256"
+                >
+                  <path d="M232,128a104,104,0,0,1-208,0c0-41,23.81-78.36,60.66-95.27a8,8,0,0,1,6.68,14.54C60.15,61.59,40,93.27,40,128a88,88,0,0,0,176,0c0-34.73-20.15-66.41-51.34-80.73a8,8,0,0,1,6.68-14.54C208.19,49.64,232,87,232,128Z"></path>
+                </svg>
+              </span>
+            )}
+          </button>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
