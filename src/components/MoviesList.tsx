46a5edadc4e1a4f36b0c477dec46990d28fef513
German Bilalov <pinncho@gmail.com>
2025-07-22 17:13:10 +0200

    style(MovieList): increase column number to 5 for large screens

--- src/components/MoviesList.tsx
+++ src/components/MoviesList.tsx
@@ -9,7 +9,7 @@ export default function MoviesList({ movies }: MoviesListProps) {
   return (
     <div className="max-w-6xl mx-auto p-4">
       <h2 className="text-3xl font-bold text-center mb-4">Movies</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
