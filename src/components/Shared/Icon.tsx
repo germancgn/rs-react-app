8c3083edb780c215c62ad0c341c73a05bb890547
German Bilalov <pinncho@gmail.com>
2025-07-26 02:42:49 +0200

    style: improve pagination button design

--- src/components/Shared/Icon.tsx
+++ src/components/Shared/Icon.tsx
@@ -6,6 +6,36 @@ interface IconProps extends React.SVGAttributes<SVGSVGElement> {
   size?: number;
 }
 
+function CaretLeft({ size = 16, ...props }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      fill="currentColor"
+      viewBox="0 0 256 256"
+      {...props}
+    >
+      <path d="M165.66,202.34a8,8,0,0,1-11.32,11.32l-80-80a8,8,0,0,1,0-11.32l80-80a8,8,0,0,1,11.32,11.32L91.31,128Z"></path>
+    </svg>
+  );
+}
+
+function CaretRight({ size = 16, ...props }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      fill="currentColor"
+      viewBox="0 0 256 256"
+      {...props}
+    >
+      <path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path>
+    </svg>
+  );
+}
+
 function MagnifyingGlass({ size = 16, ...props }: IconProps) {
   return (
     <svg
@@ -66,4 +96,4 @@ export function X({ size, ...props }: IconProps) {
   );
 }
 
-export { KeyReturn, MagnifyingGlass, StarSolid };
+export { CaretLeft, CaretRight, KeyReturn, MagnifyingGlass, StarSolid };
--- src/components/Shared/Pagination.tsx
+++ src/components/Shared/Pagination.tsx
@@ -1,3 +1,5 @@
+import { CaretLeft, CaretRight } from './Icon';
+
 type PaginationProps = {
   page: number;
   totalPages: number;
@@ -12,24 +14,26 @@ export function Pagination({
   onPrevPage,
 }: PaginationProps) {
   return (
-    <div className="flex justify-center items-center space-x-4 mt-4">
-      <button
-        onClick={onPrevPage}
-        disabled={page === 1}
-        className="px-4 py-2 bg-gray-700 text-white rounded disabled:opacity-50 cursor-pointer"
-      >
-        Previous
-      </button>
-      <span className="text-gray-300">
-        Page {page} of {totalPages}
-      </span>
-      <button
-        onClick={onNextPage}
-        disabled={page === totalPages}
-        className="px-4 py-2 bg-gray-700 text-white rounded disabled:opacity-50 cursor-pointer"
-      >
-        Next
-      </button>
+    <div className="flex justify-center items-center space-x-4 mt-4 ">
+      <div className="flex items-center bg-[#222240] rounded-xl">
+        <button
+          onClick={onPrevPage}
+          disabled={page === 1}
+          className="p-4 text-white rounded-xl disabled:opacity-50 cursor-pointer hover:bg-[#303059]"
+        >
+          <CaretLeft size={20} />
+        </button>
+        <span className="text-gray-300 px-2">
+          {page} - {totalPages}
+        </span>
+        <button
+          onClick={onNextPage}
+          disabled={page === totalPages}
+          className="p-4 text-white rounded-xl disabled:opacity-50 cursor-pointer hover:bg-[#303059]"
+        >
+          <CaretRight size={20} />
+        </button>
+      </div>
     </div>
   );
 }
