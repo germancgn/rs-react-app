e3832eb866cc3c95595e8c6514b63c34f324f2f6
German Bilalov <pinncho@gmail.com>
2025-07-27 21:31:30 +0200

    feat: use NavLink for active link styling in navbar

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -1,18 +1,34 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav className="flex flex-col max-w-6xl m-auto sm:flex-row items-center justify-between gap-4 p-4 rounded-lg">
       <ul className="flex gap-8 w-full">
-        <li className="hover:text-white">
-          <Link to="/">Home</Link>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? 'text-white' : 'text-gray-300 hover:text-white'
+            }
+          >
+            Home
+          </NavLink>
         </li>
-        <li className="hover:text-white">
-          <Link to="/about">About</Link>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? 'text-white' : 'text-gray-400 hover:text-white'
+            }
+          >
+            About
+          </NavLink>
         </li>
 
-        <li className="ml-auto hover:text-white">
-          <a href="">Sign in</a>
+        <li className="ml-auto">
+          <a href="" className="text-gray-400 hover:text-white">
+            Sign in
+          </a>
         </li>
       </ul>
     </nav>
