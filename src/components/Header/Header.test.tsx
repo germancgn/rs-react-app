58939445f25688ee32d92146d5339ab82a8e1b03
German Bilalov <pinncho@gmail.com>
2025-07-28 01:18:52 +0200

    test(Header): make sure that clear search input is being called

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -99,6 +99,26 @@ describe('Header', () => {
     expect(mockOnSearch).toHaveBeenCalledTimes(1);
   });
 
+  it('calls onClearInput when clear button is clicked', () => {
+    const mockOnClearInput = vi.fn();
+    render(
+      <MemoryRouter>
+        <Header
+          searchTerm="test query"
+          onSearch={() => {}}
+          onInputChange={() => {}}
+          isLoading={false}
+          onClearInput={mockOnClearInput}
+        />
+      </MemoryRouter>
+    );
+
+    const clearButton = screen.getByTestId('clear-input-button');
+    fireEvent.click(clearButton);
+
+    expect(mockOnClearInput).toHaveBeenCalledTimes(1);
+  });
+
   it('displays FeaturedMovieCard components for trending movies', async () => {
     render(
       <MemoryRouter>
