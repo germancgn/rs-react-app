9d1a1b97ff7c3806940b369efdfdd6d515bf19d7
German Bilalov <pinncho@gmail.com>
2025-08-10 20:09:09 +0200

    test: add renderWithProviders helper function

---
+++ src/__tests__/test-utils/renderWithProviders.tsx
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render } from '@testing-library/react';
+import { ThemeProvider } from '../../contexts/ThemeProvider';
+
+const createTestQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+export function renderWithProviders(element: React.ReactElement) {
+  const client = createTestQueryClient();
+  return render(
+    <ThemeProvider>
+      <QueryClientProvider client={client}>{element}</QueryClientProvider>
+    </ThemeProvider>
+  );
+}
