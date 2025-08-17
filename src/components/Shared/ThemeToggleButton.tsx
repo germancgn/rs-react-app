4e81408d7bd9d642cd044a7d1311ff05f1e76d6f
German Bilalov <pinncho@gmail.com>
2025-08-17 13:34:43 +0200

    fix(ThemeToggleButton): use <audio> element to fix audio lag

--- src/components/Shared/ThemeToggleButton.tsx
+++ src/components/Shared/ThemeToggleButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { useThemeContext } from '../../hooks/useTheme';
 import { Moon, Sun } from './Icon';
 
@@ -8,14 +8,6 @@ export function ThemeToggleButton() {
   const { theme, toggleTheme } = useThemeContext();
   const lightOnSound = useRef<HTMLAudioElement | null>(null);
   const lightOffSound = useRef<HTMLAudioElement | null>(null);
-
-  useEffect(() => {
-    lightOnSound.current = new Audio('/sounds/light-on.mp3');
-    lightOffSound.current = new Audio('/sounds/light-off.mp3');
-
-    lightOnSound.current.load();
-    lightOffSound.current.load();
-  }, []);
 
   const handleToggle = () => {
     toggleTheme();
@@ -33,6 +25,8 @@ export function ThemeToggleButton() {
 
   return (
     <button onClick={handleToggle} className="cursor-pointer flex">
+      <audio ref={lightOnSound} src="/sounds/light-on.mp3" preload="auto" />
+      <audio ref={lightOffSound} src="/sounds/light-off.mp3" preload="auto" />
       <div className="relative w-6 h-6">
         <div
           className={`absolute inset-0 transition-all duration-300 transform ${
