00c42da367604a5b5dc54da427a56b3afd4d4875
German Bilalov <pinncho@gmail.com>
2025-08-23 17:34:07 +0200

    chore(i18n): add translations for not-found page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,17 +1,18 @@
+import { useTranslations } from 'next-intl';
 import { Link } from '../../i18n/navigation';
 
 export default function NotFound() {
+  const t = useTranslations('Pages');
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200 dark:bg-gray-900 text-gray-800 dark:text-white">
-      <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
-      <p className="mb-6 text-lg">
-        The page you are looking for does not exist.
-      </p>
+      <h1 className="text-6xl font-bold mb-4">{t('notFoundHeading')}</h1>
+      <p className="mb-6 text-lg">{t('pageDoesNotExistText')}</p>
       <Link
         href="/"
         className="px-4 py-2 bg-pink-500 hover:bg-pink-600 rounded text-white transition"
       >
-        Go to Home
+        {t('goToHomeLinkLabel')}
       </Link>
     </div>
   );
