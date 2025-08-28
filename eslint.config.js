f8041e8bbb3c2d346d10b1aa2a7d96270bf2fcb0
German Bilalov <pinncho@gmail.com>
2025-08-28 17:23:58 +0200

    chore: setup project with Vite and React

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,23 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import react from 'eslint-plugin-react';
-import tseslint from 'typescript-eslint';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
-import pluginQuery from '@tanstack/eslint-plugin-query';
-import nextPlugin from '@next/eslint-plugin-next';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
 
-export default tseslint.config(
-  { ignores: ['.next', 'dist', 'coverage'] },
+export default tseslint.config([
+  globalIgnores(['dist']),
   {
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.strict,
-      eslintPluginPrettier,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
     ],
-    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      '@tanstack/query': pluginQuery,
-      '@next/next': nextPlugin,
-    },
-    rules: {
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...nextPlugin.configs['recommended'].rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  }
-);
+  },
+])
--- src/utils/files/download.ts
+++
@@ -1,3 +0,0 @@
-export function createBlob(content: string, mimeType = 'text/plain') {
-  return new Blob([content], { type: `${mimeType};charset=utf-8;` });
-}
