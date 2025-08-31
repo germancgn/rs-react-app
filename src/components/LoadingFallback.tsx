77ed5c2b3a000d6139644c9cda7f3cbae0710296
German Bilalov <pinncho@gmail.com>
2025-08-31 20:27:30 +0200

    style: increase gap size and change spinner color

--- src/components/LoadingFallback.tsx
+++ src/components/LoadingFallback.tsx
@@ -7,11 +7,11 @@ type LoadingFallbackProps = {
 export default function LoadingFallback({ title }: LoadingFallbackProps) {
   return (
     <div className="flex justify-center gap-4 items-center min-h-64">
-      <div className="flex flex-col bg-white/5 rounded-2xl py-6 px-4 items-center gap-8">
+      <div className="flex flex-col bg-white/5 rounded-2xl py-6 px-4 items-center gap-16">
         <h3 className="text-2xl font-bold text-gray-200 animate-pulse">
           {title}
         </h3>
-        <span className="text-gray-200 animate-pulse">
+        <span className="text-gray-400 animate-pulse">
           <Spinner size={64} />
         </span>
       </div>
